4c9991cc6419ccb3a3fad1bdd317ef4b0e783270
ModithaLekkala <modithalekkala@gmail.com>
2025-03-24 12:46:55 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS  # Import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+CORS(app)  # Enable CORS for all route
 
 # Simulated flight data
 flight_data = {
